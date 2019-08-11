d0a4c3856f24cd9ac01b7593ea8737e373cb7112
Elidor00 <filippo.bartolini9@gmail.com>
2019-08-11 18:14:32 +0200

    Updated main

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,6 +7,7 @@ import linf.parser.ComplexStaticAnalysisLexer;
 import linf.parser.ComplexStaticAnalysisParser;
 import linf.statement.Block;
 import linf.utils.Environment;
+import lvm.parser.LVMLexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -28,8 +29,8 @@ public class App {
             //create lexer
             ComplexStaticAnalysisLexer lexer = new ComplexStaticAnalysisLexer(input);
 
-            ArrayList<String> lexerErrors = new ArrayList<>(); //list of lexical errors
-            //lexical errors
+            ArrayList<String> lexerErrors = new ArrayList<>(); //list of LEXICAL errors
+            //check LEXICAL errors
             for (Token t: lexer.getAllTokens()){ //get all token
                 //System.out.println("\nToken: " + "t");
                 if (t.getType() == lexer.ERR){ //check token type
@@ -42,30 +43,42 @@ public class App {
                 }
             }
             if(!lexerErrors.isEmpty()){
-                System.err.println("Found lexical errors. Exiting process.");
+                System.err.println("Found LEXICAL errors. Exiting process.");
                 exit(-1);
             }
 
-            //create linf.parser
+            lexer.reset();
+
+            //create parser
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             ComplexStaticAnalysisParser parser = new ComplexStaticAnalysisParser(tokens);
-
             parser.setBuildParseTree(true);
 
+            //create custom visitor
             LinfVisitorImpl visitor = new LinfVisitorImpl();
-
+            //start visit the root and then recursively visit the whole tree
             Block blk = (Block) visitor.visit(parser.block());
+            //check SYNTAX errors
+            if(parser.getNumberOfSyntaxErrors()!= 0){
+                System.err.println("Found SYNTAX errors. Exiting process.");
+                exit(-1);
+            }
 
             Environment env = new Environment();
 
-            ArrayList<SemanticError> errors = blk.checkSemantics(env);
-            for (SemanticError e : errors) {
-                System.out.println(e);
+            ArrayList<SemanticError> semanticsErrors = blk.checkSemantics(env); //list of SEMANTICS errors
+            //check semantics errors
+            for (SemanticError err : semanticsErrors) {
+                System.out.println(err);
+            exit(-1);
             }
-
-            if (errors.isEmpty()) {
+            //no SEMANTICS errors
+            if (semanticsErrors.isEmpty()) {
                 try {
+                    //typeCheck
                     blk.checkType();
+                    // TODO: cgen
+                    // TODO: added interpeter
                 } catch (TypeError e) {
                     e.printStackTrace();
                 }
