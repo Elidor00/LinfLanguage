2436120aaa02dc63b434978df5ec740cbafb791c
Elidor00 <filippo.bartolini9@gmail.com>
2019-08-17 12:16:41 +0200

    Update env

--- src/main/java/linf/utils/Environment.java
+++ src/main/java/linf/utils/Environment.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 
 public class Environment {
     public int nestingLevel = -1;
-    int offset = 0;
+    public int offset = 0;
     // List of hash tables
     private final LinkedList<HashMap<String, STentry>> symbolsTable = new LinkedList<>();
     public int nestingLevel = -1;
