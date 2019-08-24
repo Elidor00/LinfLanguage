b1e99f89bac1683379cd74401905a001c40d5a4a
Elidor00 <filippo.bartolini9@gmail.com>
2019-08-24 23:45:14 +0200

    Updated Assignment tests

--- src/test/java/type/AssignmentTest.java
+++ src/test/java/type/AssignmentTest.java
@@ -1,4 +1,66 @@
 package type;
 
+import linf.error.type.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+import static utils.TestUtils.checkType;
+
+@RunWith(JUnit4.class)
 public class AssignmentTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void CheckType_ShouldPass_SimpleIntAssignment() throws TypeError {
+        checkType(
+                "{" +
+                        "int x = 2;" +
+                        "int y = 42;" +
+                        "}"
+        );
+    }
+
+    @Test
+    public void CheckType_ShouldPass_SimpleBoolAssignment() throws TypeError {
+        checkType(
+                "{" +
+                        "bool x = true;" +
+                        "bool y = false;" +
+                        "}"
+        );
+    }
+
+    /*
+    @Test (expected = IncompatibleTypesError.class)
+    //(expected=Exception.class)
+    public void CheckType_ShouldFail_WithIncompatibleTypes() throws TypeError {
+        checkType(
+                "{int x = true;}"
+        );
+    }
+    */
+
+    @Test
+    public void CheckType_ShouldFail_WithIncompatibleTypesBoolInt() throws TypeError {
+        exception.expect(IncompatibleTypesError.class);
+        checkType(
+                "{bool x = 2;}"
+        );
+    }
+
+    @Test
+    public void CheckType_ShouldFail_WithIncompatibleTypesIntBool() throws TypeError {
+        exception.expect(IncompatibleTypesError.class);
+        checkType(
+                "{int x = true;}"
+        );
+    }
+
 }
+
