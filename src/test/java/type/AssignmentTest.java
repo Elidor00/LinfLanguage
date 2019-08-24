07dc9180ca7ebf2c111b93a210e8aeb8196fedac
Elidor00 <filippo.bartolini9@gmail.com>
2019-08-25 00:21:05 +0200

    Updated FunDec FunCall tests

--- src/test/java/type/AssignmentTest.java
+++ src/test/java/type/AssignmentTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import static org.junit.Assert.*;
 import static utils.TestUtils.checkType;
 
 @RunWith(JUnit4.class)
