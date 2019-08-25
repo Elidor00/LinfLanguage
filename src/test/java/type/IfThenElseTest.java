7c85f7153b5d28fdcab0c8a0b8a6edd8ee58604c
Elidor00 <filippo.bartolini9@gmail.com>
2019-08-25 13:50:54 +0200

    Updated IfThenElse tests

---
+++ src/test/java/type/IfThenElseTest.java
@@ -0,0 +1,47 @@
+package type;
+
+import linf.error.type.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static utils.TestUtils.checkType;
+
+@RunWith(JUnit4.class)
+public class IfThenElseTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void CheckType_ShouldPass_SimpleIfThenElse() throws TypeError {
+        checkType(
+                "{" +
+                        "int x = 1;" +
+                        "if (x == 1) then {" +
+                        "x = x + 1;}" +
+                        "else {" +
+                        "x = x - 1;" +
+                        "}" +
+                        "}"
+        );
+    }
+
+    @Test
+    public void CheckType_ShouldFail_UnbalanceDeletionBehaviourError() throws TypeError {
+        exception.expect(UnbalancedDeletionBehaviourError.class);
+        checkType(
+                "{" +
+                        "int x = 1;" +
+                        "bool y = true;" +
+                        "if (x == 1) then {" +
+                        "delete x;" +
+                        "} else { " +
+                        "delete y;" +
+                        "}" +
+                        "}"
+        );
+    }
+
+}
