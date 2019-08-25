4afa90dacd30fafdd1d3d28e1904b15c96d5bdbf
Elidor00 <filippo.bartolini9@gmail.com>
2019-08-25 13:51:52 +0200

    Updated FunCall tests

--- src/test/java/type/FunCallTest.java
+++ src/test/java/type/FunCallTest.java
@@ -113,5 +113,20 @@ public class FunCallTest {
         );
     }
 
+    @Test
+    public void CheckType_ShouldFail() throws TypeError {
+        exception.expect(IncompatibleBehaviourError.class);
+        checkType(
+                "{" +
+                        "int y = 1;" +
+                        "f() {" +
+                        "y = y + 4;" +
+                        "delete y;" +
+                        "}" +
+                        "f();" +
+                        "}"
+        );
+    }
+
 }
 
