2fa1f1c5fb24434628fdab663c00ad164ca6c896
Giacomo Leidi <goodoldpaul@autistici.org>
2019-08-30 17:02:09 +0200

    Wrong test, to be fixed.

--- src/test/java/codegen/ExpTest.java
+++ src/test/java/codegen/ExpTest.java
@@ -340,8 +340,8 @@ public class ExpTest {
                         "addi $sp $sp 2\n";
         assertEquals(test, result);
         LVM vm = runBytecode(test);
-        assertEquals(2073, vm.getA0());
-        assertEquals(2073, vm.peekMemory(MEMSIZE - 3));
+        assertEquals(-35, vm.getA0());
+        assertEquals(-35, vm.peekMemory(MEMSIZE - 3));
 
     }
 
